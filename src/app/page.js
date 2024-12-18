c7a15507659f308b58ad35fe951c0d0bb3da0c9d
vidwan <163408880+vidwanns@users.noreply.github.com>
2024-12-19 00:27:13 +0530

    second commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Header from "./components/layout/header";
 import FirstSection from "./components/layout/firstSection";
+import SecondSection from "./components/layout/secondSection";
 
 function App() {
   return (
     <div>
       <Header />
       <FirstSection />
+      <SecondSection />
     </div>
   );
 }
