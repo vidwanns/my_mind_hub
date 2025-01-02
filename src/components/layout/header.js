fcabb224bf9cec4a11450f3e6fe8eb33348a3f58
vidwan <163408880+vidwanns@users.noreply.github.com>
2025-01-02 22:43:34 +0530

    header

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -3,27 +3,39 @@ import "../../styles/layout-css/header.css";
 
 const Header = () => {
   return (
-    <header className="header">
-  <div className="header__left">
-    
-    <div className="header__logo">
-      <img src="/images/home/header/mindhub_logo.svg" alt="My Mind Hub Logo" className="logo-icon" />
+    <div className="container">
+      <header className="header">
+        <div className="header__left">
+          <div className="header__logo">
+            <img
+              src="/images/home/header/mindhub_logo.svg"
+              alt="My Mind Hub Logo"
+              className="logo-icon"
+            />
+          </div>
+
+          <div className="vertical-line"></div>
+
+          <nav className="header__nav">
+            <a href="#about" className="nav-link">
+              About
+            </a>
+            <a href="#contact" className="nav-link">
+              Contact
+            </a>
+            <a href="#list" className="nav-link">
+              List Your Practice
+            </a>
+          </nav>
+        </div>
+
+        <div className="header__cta">
+          <a href="#find-therapist" className="find-therapist-button">
+            Find A Therapist
+          </a>
+        </div>
+      </header>
     </div>
-
-    <div className="vertical-line"></div>
-
-    <nav className="header__nav">
-      <a href="#about" className="nav-link">About</a>
-      <a href="#contact" className="nav-link">Contact</a>
-      <a href="#list" className="nav-link">List Your Practice</a>
-    </nav>
-  </div>
-
-  <div className="header__cta">
-    <a href="#find-therapist" className="find-therapist-button">Find A Therapist</a>
-  </div>
-</header>
-
   );
 };
 
