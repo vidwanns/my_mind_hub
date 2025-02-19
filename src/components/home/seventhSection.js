1333020bb20bb810f0eb7523d7e104a24fd2688e
vidwan <163408880+vidwanns@users.noreply.github.com>
2025-02-20 01:12:13 +0530

    seven lap

--- src/components/home/seventhSection.js
+++ src/components/home/seventhSection.js
@@ -6,42 +6,42 @@ import "../../styles/home-css/seventhSection.css";
 const reviews = [
   {
     id: 1,
-    text: "Mind Hub transformed my mental health journey! Easy navigation, detailed profiles, and seamless scheduling helped me find the right therapist effortlessly. Grateful for their support on my journey!",
+    text: `Mind Hub transformed my mental health journey! Easy navigation, detailed profiles, and seamless scheduling helped me find the right therapist effortlessly. Grateful for their support on my journey!`,
     name: "Kayla",
     image: "/images/home/seventhSection/kayla.png",
     color: "#C5E7AD",
   },
   {
     id: 2,
-    text: "Mind Hub stands out as the number one choice for its empowering and supportive platform. It has made a positive impact on my mental health journey, and I'm truly thankful for this invaluable resource.",
+    text: `Mind Hub stands out as the number one choice for its empowering and supportive platform. It has made a positive impact on my mental health journey, and I'm truly thankful for this invaluable resource.`,
     name: "Eric",
     image: "/images/home/seventhSection/eric.png",
     color: "#B8DCE4",
   },
   {
     id: 3,
-    text: "Mind Hub is a lifesaver for mental health professionals. The convenience, information, and support provided are invaluable. Thank you, Mind Hub, for being a beacon of hope.",
+    text: `Mind Hub is a lifesaver for mental health professionals. The convenience, information, and support provided are invaluable. Thank you, Mind Hub, for being a beacon of hope.`,
     name: "Clara",
     image: "/images/home/seventhSection/clara.png",
     color: "#DAEE96",
   },
   {
     id: 4,
-    text: "With Mind Hub, I found a therapist who specializes in my specific needs. The global service and variety are impressive!",
+    text: `With Mind Hub, I found a therapist who specializes in my specific needs. The global service and variety are impressive!`,
     name: "Mel",
     image: "/images/home/seventhSection/mel.png",
     color: "#F4E0B4",
   },
   {
     id: 5,
-    text: "The platform is fantastic! I found a therapist in minutes, and the care I received was beyond my expectations. Mind Hub Directory is excellent.",
+    text: `The platform is fantastic! I found a therapist in minutes, and the care I received was beyond my expectations. Mind Hub Directory is excellent.`,
     name: "Matt",
     image: "/images/home/seventhSection/matt.png",
     color: "#DCDCCD",
   },
   {
     id: 6,
-    text: "The platform is fantastic! I found a therapist and received personalized care. Mind Hub Directory is excellent.",
+    text: `The platform is fantastic! I found a therapist and received personalized care. Mind Hub Directory is excellent.`,
     name: "Amy",
     image: "/images/home/seventhSection/amy.png",
     color: "#E9CBB5",
@@ -53,13 +53,13 @@ const SeventhSection = () => {
 
   return (
     <section className="seventh-section">
-      <h3 className="section-subheading">Client Reviews</h3>
-      <h2 className="section-heading">
+      <p className="section-subheading">Client Reviews</p>
+      <p className="section-heading">
         A Few Words From <span className="highlight">Our Customers</span>
-      </h2>
+      </p>
 
       <div className="carousel-container">
-        {/* First Row with Left Shift */}
+        {/* First Row */}
         <div className="carousel-row-container">
           <div className="carousel-row">
             {repeatedReviews.map((review, index) => (
@@ -76,7 +76,7 @@ const SeventhSection = () => {
                   />
                 </div>
                 <div className="review-content">
-                  <p className="review-text">"{review.text}"</p>
+                  <p className="review-text">{review.text}</p>
                   <p className="review-author-name">- {review.name}</p>
                 </div>
               </div>
@@ -101,7 +101,7 @@ const SeventhSection = () => {
                   />
                 </div>
                 <div className="review-content">
-                  <p className="review-text">"{review.text}"</p>
+                  <p className="review-text">{review.text}</p>
                   <p className="review-author-name">- {review.name}</p>
                 </div>
               </div>
@@ -126,7 +126,7 @@ const SeventhSection = () => {
                   />
                 </div>
                 <div className="review-content">
-                  <p className="review-text">"{review.text}"</p>
+                  <p className="review-text">{review.text}</p>
                   <p className="review-author-name">- {review.name}</p>
                 </div>
               </div>
