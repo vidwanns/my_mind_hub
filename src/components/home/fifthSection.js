864e6334c7f5df2ddb82c17a3deed363a2f75f49
vidwan <163408880+vidwanns@users.noreply.github.com>
2025-02-20 00:47:39 +0530

    fifth lap

--- src/components/home/fifthSection.js
+++ src/components/home/fifthSection.js
@@ -7,11 +7,11 @@ const FifthSection = () => {
       {/* First Block */}
       <div className="content-block">
         <div className="content-text">
-          <h3>Find A Mental Health Practitioner</h3>
-          <h2>
+          <p className="content-heading">Find A Mental Health Practitioner</p>
+          <p className="content-subheading">
             Get In Touch With <span className="highlight">A Trusted Mental Health</span> Professional Online Or In Your Area
-          </h2>
-          <p>
+          </p>
+          <p className="content-description">
             Finding support is easy with Mind Hub Directory. Explore therapists, learn about their qualifications, and request help online. Healing happens in different steps, and on our platform, you’ll discover a therapist for every part of your journey.
           </p>
           <button className="fifth_cta-button">Search Therapists</button>
@@ -27,11 +27,11 @@ const FifthSection = () => {
           <img src="/images/home/fifthSection/girl2.png" alt="Mental Health Support" />
         </div>
         <div className="content-text">
-          <h3>Mental Health Care Is Self Care</h3>
-          <h2>
+          <p className="content-heading">Mental Health Care Is Self Care</p>
+          <p className="content-subheading">
             Everyone Benefits From <span className="highlight">Mental Health Support</span>
-          </h2>
-          <p>
+          </p>
+          <p className="content-description">
             For a rapid mental health check or self-care, explore our list of mental health practitioners on Mind Hub Directory. In our fast-paced world, taking a moment for self-care is crucial.
           </p>
           <button className="fifth_cta-button">Book A Session</button>
